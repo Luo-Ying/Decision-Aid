4f2c995788aa9fba6b14094d6debe0e056ab7367
yluo <yluo@orkis.com>
2024-04-09 19:47:37 +0200

    Nothing update

--- Create_seuils.py
+++ Create_seuils.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Charger les données pour obtenir les valeurs maximales et minimales pour chaque critère
-data_df = pd.read_csv('Donnees/Fat_Data.csv', index_col = 0)
-data_weight = pd.read_csv('Donnees/Fat_poids.csv', index_col = 0)
+data_df = pd.read_csv("Donnees/Fat_Data.csv", index_col=0)
+data_weight = pd.read_csv("Donnees/Fat_poids.csv", index_col=0)
 
 # Calculer la plage des valeurs pour chaque critère
 range_values = (data_df.median()).round(5)
@@ -24,45 +24,72 @@ default_seuil = 0.15
 
 # Mapping des critères par catégorie (à adapter avec les vrais noms des critères)
 categorie_mapping = {
-    'Fruits': ['Fruits - Excluding Wine', 'Treenuts'],
-    'Legumes': ['Vegetables', 'Starchy Roots', 'Pulses', 'Olives', 'Oilcrops', 'Cereals - Excluding Beer','Spices','Sugar Crops','Vegetable Oils','Vegetal Products'],
-    'Viande': ['Meat', 'Offals', 'Animal Products', 'Fish, Seafood', 'Animal fats', 'Eggs']
+    "Fruits": ["Fruits - Excluding Wine", "Treenuts"],
+    "Legumes": [
+        "Vegetables",
+        "Starchy Roots",
+        "Pulses",
+        "Olives",
+        "Oilcrops",
+        "Cereals - Excluding Beer",
+        "Spices",
+        "Sugar Crops",
+        "Vegetable Oils",
+        "Vegetal Products",
+    ],
+    "Viande": [
+        "Meat",
+        "Offals",
+        "Animal Products",
+        "Fish, Seafood",
+        "Animal fats",
+        "Eggs",
+    ],
 }
 
 # Définir les seuils en pourcentage pour chaque profil en fonction de enverser la proportion de leur préférence (poids)
-seuil_profil1_favoris = data_weight.min(axis=1)['Profil 1']
-seuil_profil1_non_favoris = data_weight.max(axis=1)['Profil 1']
-seuil_profil3_favoris = data_weight.min(axis=1)['Profil 3']
-seuil_profil3_non_favoris = data_weight.max(axis=1)['Profil 3']
-default_seuil = data_weight.mean(axis=1)['Profil 2']
+seuil_profil1_favoris = data_weight.min(axis=1)["Profil 1"]
+seuil_profil1_non_favoris = data_weight.max(axis=1)["Profil 1"]
+seuil_profil3_favoris = data_weight.min(axis=1)["Profil 3"]
+seuil_profil3_non_favoris = data_weight.max(axis=1)["Profil 3"]
+default_seuil = data_weight.mean(axis=1)["Profil 2"]
+
 
 # Fonction pour déterminer le seuil pour un critère donné
 def determine_seuil(critere, profil):
     for categorie, criteres in categorie_mapping.items():
         if critere in criteres:
-            if profil == 1 and categorie in ['Fruits', 'Legumes']:
+            if profil == 1 and categorie in ["Fruits", "Legumes"]:
                 return range_values[critere] * seuil_profil1_favoris
-            elif profil == 1 and categorie == 'Viande':
+            elif profil == 1 and categorie == "Viande":
                 return range_values[critere] * seuil_profil1_non_favoris
-            elif profil == 3 and categorie == 'Viande':
+            elif profil == 3 and categorie == "Viande":
                 return range_values[critere] * seuil_profil3
-            elif profil == 3 and categorie == ['Fruits', 'Legumes']:
+            elif profil == 3 and categorie == ["Fruits", "Legumes"]:
                 return range_values[critere] * seuil_profil3
     return range_values[critere] * default_seuil
 
+
 # Calculer les seuils pour chaque profil et critère
-seuils_profil1 = {critere: determine_seuil(critere, 1) for critere in data_df.columns[0:]}
-seuils_profil2 = {critere: determine_seuil(critere, 2) for critere in data_df.columns[0:]}
-seuils_profil3 = {critere: determine_seuil(critere, 3) for critere in data_df.columns[0:]}
+seuils_profil1 = {
+    critere: determine_seuil(critere, 1) for critere in data_df.columns[0:]
+}
+seuils_profil2 = {
+    critere: determine_seuil(critere, 2) for critere in data_df.columns[0:]
+}
+seuils_profil3 = {
+    critere: determine_seuil(critere, 3) for critere in data_df.columns[0:]
+}
 
-# Afficher les seuils
-# print('Seuils Profil 1:', seuils_profil1)
-# print('Seuils Profil 2:', seuils_profil2)
-# print('Seuils Profil 3:', seuils_profil3)
-
-seuils_df = pd.DataFrame({
-    'Seuil 1': seuils_profil1,
-    'Seuil 2': seuils_profil2,
-    'Seuil 3': seuils_profil3
-}).round(5).T
-seuils_df.to_csv('Fat_seuils.csv', index_label='Seuils')
+seuils_df = (
+    pd.DataFrame(
+        {
+            "Seuil 1": seuils_profil1,
+            "Seuil 2": seuils_profil2,
+            "Seuil 3": seuils_profil3,
+        }
+    )
+    .round(5)
+    .T
+)
+seuils_df.to_csv("Fat_seuils.csv", index_label="Seuils")
