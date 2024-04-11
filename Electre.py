bd2ea573be248f1981bba033345854e3a43295ae
yluo <yluo@orkis.com>
2024-04-11 09:34:15 +0200

    Add function to calcul table veto

--- Electre.py
+++ Electre.py
@@ -3,25 +3,23 @@ import numpy as np
 import csv
 
 """Avec les données de test de TD2"""
-# data_path = 'Donnees/testdata.csv'
-# data_df = pd.read_csv(data_path)
-# objectives_df = data_df[data_df['categorie'] == 'objectif']
-# weights_df = data_df[data_df['categorie'] == 'poids']
-# seuil_df = data_df[data_df['categorie'] == 'seuil']
-# data_df = data_df[data_df['categorie'] != 'objectif']
-# data_df = data_df[data_df['categorie'] != 'poids']
-# data_df = data_df[data_df['categorie'] != 'seuil']
+data_path = 'Donnees/Test_Data.csv'
+weights_path = 'Donnees/Test_poids.csv'
+objectives_path = 'Donnees/Test_objective.csv'
+seuil_path = 'Donnees/Test_seuils.csv'
+veto_path = 'Donnees/Test_veto.csv'
 
 """Avec les données de Fat_Data.csv"""
-data_path = 'Donnees/Fat_Data.csv'
-weights_path = 'Donnees/Fat_poids.csv'
-objectives_path = 'Donnees/Fat_objective.csv'
-seuil_path = 'Donnees/Fat_seuils.csv'
+# data_path = 'Donnees/Fat_Data.csv'
+# weights_path = 'Donnees/Fat_poids.csv'
+# objectives_path = 'Donnees/Fat_objective.csv'
+# seuil_path = 'Donnees/Fat_seuils.csv'
 
 data_df = pd.read_csv(data_path)
 weights_df = pd.read_csv(weights_path)
 objectives_df = pd.read_csv(objectives_path)
 seuil_df=pd.read_csv(seuil_path)
+veto_df=pd.read_csv(veto_path)
 
 def distance_diff(data_candidate1_value, data_candidate2_value, criterion_weight_value, seuil_data_value):
     data_candidate1_value = float(data_candidate1_value)
@@ -37,6 +35,7 @@ def distance_diff(data_candidate1_value, data_candidate2_value, criterion_weight
 # Fonction pour calculer le degré de préférence univarié
 def preference_degree(data_candidate1, data_candidate2, criterion_weight, criterion_objectif, seuil=False, seuil_value = 0):
     preference_degreeSum = 0
+    criterion_weight = criterion_weight.iloc[:, 1:]
     for criterion in criterion_weight:
         if criterion_objectif[criterion].iloc[0].lower() == 'max':
             if float(data_candidate1[criterion]) >= float(data_candidate2[criterion]):
@@ -68,44 +67,68 @@ def multicriteria_preference(criterion_weight, criterion_objectif, seuil=False, 
     
     return preference_table
 
+def get_veto(data_candidate1, data_candidate2, criterion_weight, criterion_objectif, criterion_veto):
+    veto = False
+    criterion_weight = criterion_weight.iloc[:, 1:]
+    for criterion in criterion_weight:
+        diff = abs(float(data_candidate1[criterion]) - float(data_candidate2[criterion]))
+        if criterion_objectif[criterion].iloc[0].lower() == 'max':
+            if float(data_candidate1[criterion]) < float(data_candidate2[criterion]) and diff > float(criterion_veto[criterion]):
+                veto = True
+        elif criterion_objectif[criterion].iloc[0].lower() == 'min':
+            if float(data_candidate1[criterion]) > float(data_candidate2[criterion]) and diff > float(criterion_veto[criterion]):
+                veto = True
+    return veto
 
+def multicriteria_veto(criterion_weight, criterion_objectif, criterion_veto):
+    n = data_df.shape[0]
+    veto_table = np.full((n, n), None)
+    for i in range(n):
+        for j in range(n):
+            print(f"{i}, {j}")
+            veto_table[i][j] = get_veto(data_df.iloc[i], data_df.iloc[j], criterion_weight, criterion_objectif, criterion_veto) if i != j else None
+    return veto_table
 
-def calcul_for_all_profile(lstProfil, nameColumnWeight, nameColumnObjective, seuil=False, nameColumnSeuil="Seuils"):
+def calcul_for_all_profile(lstProfil, nameColumnWeight, nameColumnObjective, seuil=False, nameColumnSeuil="Seuils", veto=False, nameColumnVeto="Veto"):
     
     rsl_multicriteria_preference = {}
+    rsl_veto = {}
     
     for profil in lstProfil:
         rsl_multicriteria_preference[profil] = multicriteria_preference(weights_df[weights_df[nameColumnWeight] == profil], objectives_df[objectives_df[nameColumnObjective] == profil], seuil, seuil_df[seuil_df[nameColumnSeuil] == profil])
-        
-    return rsl_multicriteria_preference
+        if veto:
+            rsl_veto[profil] = multicriteria_veto(weights_df[weights_df[nameColumnWeight] == profil],objectives_df[objectives_df[nameColumnObjective] == profil], veto_df[veto_df[nameColumnVeto] == profil])
+    return rsl_multicriteria_preference, rsl_veto
     
     
     
 def main():
  
     """Avec les données de test de TD2"""
-    # test_data_rslt = multicriteria_preference(weights_df,objectives_df, False)
-    # test_data_rslt_seuil = multicriteria_preference(weights_df,objectives_df,True, seuil_df)
-    # print(test_data_rslt)
-    # print()
-    # print(test_data_rslt_seuil)
+    lstProfils = weights_df['Poids']
+    print(weights_df['Poids'])
+    # rslt = calcul_for_all_profile(lstProfils, 'Poids', 'Objectif', False)
+    rslt_with_seuil = calcul_for_all_profile(lstProfils, 'Poids', 'Objectif', True, 'Seuils', True, 'Veto')
+    # print(rslt)
+    print()
+    print(rslt_with_seuil)
 
 
     """Avec les données de Fat_Data.csv"""
-    candidates = data_df['Country']
-    criteria = data_df.columns[1:] # La première colonne est celle des candidats
-    lstProfils = weights_df['Poids']
+    # candidates = data_df['Country']
+    # criteria = data_df.columns[1:] # La première colonne est celle des candidats
+    # lstProfils = weights_df['Poids']
     
-    rslt = calcul_for_all_profile(lstProfils, 'Poids', 'Poids', False)
-    rslt_with_seuil = calcul_for_all_profile(lstProfils, 'Poids', 'Poids', True, 'Seuils')
+    # rslt = calcul_for_all_profile(lstProfils, 'Poids', 'Poids', False)
+    # rslt_with_seuil = calcul_for_all_profile(lstProfils, 'Poids', 'Poids', True, 'Seuils')
     
-    print(rslt['Profil 1'])
-    print(rslt['Profil 2'])
-    print(rslt['Profil 3'])
+    # print(rslt['Profil 1'])
+    # print(rslt['Profil 2'])
+    # print(rslt['Profil 3'])
     
-    print(rslt_with_seuil['Profil 1'])
-    print(rslt_with_seuil['Profil 2'])
-    print(rslt_with_seuil['Profil 3'])
+    # print(rslt_with_seuil['Profil 1'])
+    # print(rslt_with_seuil['Profil 2'])
+    # print(rslt_with_seuil['Profil 3'])
 
 
     
